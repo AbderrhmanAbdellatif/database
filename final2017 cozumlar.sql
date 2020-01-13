24c557f80677ab7e8e2e458d1e27e3a1cf9d6a35
14MST <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2020-01-13 17:16:46 +0300

    Create final2017 cozumlar.sql

---
+++ final2017 cozumlar.sql
@@ -0,0 +1,24 @@
+1. (12) Aşağıdaki tanımları yapınız. 
+a. Veri ambarı sistemlerin temel özelliklerini ve 
+  klasik ilişkisel yaklaşımdan farklarını açıklayınız.
+b. Varlık ilişki diyagramı ve ilişkisel veri modeli arasındaki
+  farkları kullanım amacını düşünerek açıklayınız. 
+c. İlişkisel modele geçişte birebir ve çoktan çoğa ilişkiler nasıl çözülebilir.
+ Açıklayınız. 
+ 
+a) Datalar veri ambarında küplerde bulunur ve bu küplerde datalar her 
+   türlü sonucu vericek şekilde saklanır ama ilişkisel modelde datalar
+   tablolarda saklıdır ve istediğim istenilen sonuç için SQL
+    sorguları yazılmalıdır küplerde ise bunlar hazırdır.
+b) Varlık diyagramı varlıkların ilişkisel modele çevrilmesinde yardımcı olur. 
+     Öncelikle varlık diyagramı çizilir ilişkiler belli olur ve bu varlık modeline göre
+     ilişkisel modele çevrilir.
+c) Varlık ilişki diyagramındaki çoğa çok ilişkiler ilişkisel veritabanında 
+oluşturulamaz. Hangi kayıtların silişkili olduğu bilgisi 
+     ara bir tabloda saklanır. İki varlık da bağlantı tablosuna 
+	 ayrı ayrı birden çoğa ilişkiyle bağlanır 
+     Birebir ilişkinin de ilişkisel modelde karşılığı yoktur.
+	 Bu ilişki  analizdeki ihtiyaca göre varlıkların tek tabloda birleştirilmesi
+     ,birincil anahtarların aynı yapılması veya kodlamayla çözülebilir
+ 	 
+2. (33) Aşağıdaki soruları verilen veri modeli göre cevaplandırınız
